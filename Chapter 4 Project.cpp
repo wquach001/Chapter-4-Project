52082fcb8df252bd3ec5494d98fd33f73be078ff
Wai Quach <wyquach5@gmail.com>
2025-07-09 19:20:29 -0400

    Add project files.

---
+++ Chapter 4 Project.cpp
@@ -0,0 +1,73 @@
+// Chapter 4 Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+/*
+	int month, year;
+    cout << "Please enter the month usng integers 1-12: " << "\n";
+    cin >> month;
+    cout << "Please enter the year: " << "\n";
+    cin >> year;
+    if (month <= 12 && month > 0)
+    {
+        if (month == 1)
+        {
+            cout << "31 days" << "\n";
+        }
+        else if ((month >= 3 && month <= 7) || (month >= 9 && month <= 12))
+        {
+            if (month % 2 == 0)
+            {
+                cout << "30 days" << "\n";
+            }
+            else
+            {
+                cout << "31 days" << "\n";
+            }
+        }
+        else if (month == 8)
+        {
+            cout << "31 days" << "\n";
+        }
+        else if (month == 2)
+        {
+            if (year % 100 == 0 && year % 400 == 0)
+            {
+                cout << "29 days" << "\n"; // Leap year
+            }
+            else if (year % 4 == 0)
+            {
+			    cout << "29 days" << "\n"; // Leap year
+            }
+            else
+            {
+                cout << "28 days" << "\n"; // Non-leap year
+            }
+        }
+    }
+
+	if (month < 1 || month > 12)
+    {
+        cout << "Invalid month. Please enter a number between 1 and 12." << "\n";
+		return 1; // Exit the program with an error code
+    
+    }
+*/
+    
+	return 0; // Exit the program successfully
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
